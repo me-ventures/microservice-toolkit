1e2c55aa7b0b6afc462f4546458c36350add1624
M. Hofman <m.hofman@chronos-technology.nl>
2016-07-15 11:44:31 +0200

    Make it possible to init modules separately [Finishes #126379323]

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 module.exports = {
-    init: init
+    /**
+     * @deprecated Use the separate initialization inside the modules.
+     */
+    init: init,
+    http: require('./src/http'),
+    metrics: require('./src/metrics'),
+    context: require('./src/context')
 };
 
+/**
+ *
+ * @param config
+ * @returns {{http: *, metrics: *, context: *}}
+ * @deprecated
+ */
 function init( config ){
     return {
         http: require('./src/http'),
