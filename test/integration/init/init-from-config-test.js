9255942f709247d56105d27c85c4c1090ead7c10
EvaLok <squidstock@hotmail.com>
2016-09-21 08:53:43 -0600

    tests for auth module

--- test/integration/init/init-from-config-test.js
+++ test/integration/init/init-from-config-test.js
@@ -2,6 +2,7 @@ var assert = require('chai').assert;
 var sut = require('../../../index');
 var fs = require('fs');
 var request = require('request');
+var nock = require('nock');
 
 describe('toolkit.initFromConfig', function(){
     it('should initialize all components present in supplied config', function(done){
@@ -68,6 +69,40 @@ describe('toolkit.initFromConfig', function(){
 
             done();
         });
+    });
+
+    it('should initialize permissions functionality when present in config', function(done){
+        this.timeout = 10000;
+
+        var authEndpoint1 = nock('http://authorization')
+            .get('/v1/user/123/hasPermission/test-operation')
+            .reply(200);
+
+        var authEndpoint2 = nock('http://authorization')
+            .get('/v1/user/123/hasPermission/test-operation-2')
+            .reply(404);
+
+        sut.initFromConfig({
+            "name": "public-product-frontend-layer",
+            "authorization": {
+                endpoint: "http://authorization"
+            }
+        });
+
+        sut.authorizaion.checkPermission('test-operation', 123)
+            .then(result => {
+                assert.isTrue(authEndpoint1.isDone());
+                assert.isTrue(result);
+
+                sut.authorizaion.checkPermission('test-operation-2', 123)
+                    .then(result => {
+                        assert.isTrue(authEndpoint2.isDone());
+                        assert.isFalse(result);
+
+                        return done();
+                    })
+            })
+            .catch(console.error);
     });
 });
 
